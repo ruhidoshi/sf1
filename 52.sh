090f73b0b06a1f18346140c25b72bae81bd474c3
ruhidoshi <141505284+ruhidoshi@users.noreply.github.com>
2023-12-18 00:27:02 +0530

    52.sh

--- 52.sh
+++ 52.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-echo Enter Directory Name
-read d
-echo menu
-echo 1. Short file description 
-echo 2. Long file description 
-echo 3. Hidden files
-echo Enter your choice
-read a
-case $a in
-1) ls $d
-;;
-2) ls -l $d
-;;
-3) ls -d $d
-;;
-*) echo invalid choice
-;;
+
+echo "Enter Directory or File Path"
+read -r path
+
+if [ ! -e "$path" ]; then
+    echo "File or directory does not exist."
+    exit 1
+fi
+
+echo "Menu:"
+echo "1. Short file description"
+echo "2. Long file description"
+echo "3. Hidden files"
+echo "Enter your choice"
+read -r choice
+
+case "$choice" in
+    1) echo "Short file description:"
+       ls -1 "$path";;
+    2) echo "Long file description:"
+       ls -l "$path";;
+    3) echo "Hidden files:"
+       ls -a "$path";;
+    *) echo "Invalid choice";;
 esac
